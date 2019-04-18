4c1c066c91415d7e2cd05d2517d66b60fd54a1eb
Guilherme Dantas <guidanoli@hotmail.com>
2019-04-18 08:20:36 -0300

    vers

--- psalm_versicule_per_line.py
+++ psalm_versicule_per_line.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 
 try:
     f = open("psalms_unformatted.txt","r")
@@ -8,3 +8,22 @@ except IOError:
     print("Couldn't read Psalms text file.")
     sys.exit()
 
+pat = re.compile("\d+:\d+")
+vers = []
+old_end = 0
+for n in pat.finditer(s):
+    v = s[old_end:n.start()].strip()
+    if len(v) > 0:
+        vers.append(v.replace('\n',' '))
+    old_end = n.end()
+if len(s[old_end:].strip()) > 0:
+    vers.append(s[old_end:].strip().replace('\n',' '))
+
+try:
+    f = open("psalms_vers.txt","w")
+    s = f.write('\n'.join(vers))
+    f.close()
+except IOError:
+    print("Couldn't write formatted Psalms text file.")
+    sys.exit()
+    
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+set /p file="filename (press 'tab' to show options): "
+python %file%
+pause
